d0d2bfff6669ed47d28292da8a93a3ee6f5211ad
callumhemming <callum.m.hemming@outlook.com>
2022-06-25 03:02:52 +0100

    Removed OSC CMS files

--- pages/api/database/cms.ts
+++ pages/api/database/cms.ts
@@ -1,20 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import getCMSfile from "../../../lib/models/getCMSfile";
+import updateCMSfile from "../../../lib/models/updateCMSfile";
 
 interface apiResponse {
-    success: boolean;
-    payload: any[];
-  }
+  success: boolean;
+  payload: any[];
+}
 
 export default async function Handler(
-    req: NextApiRequest,
-    res: NextApiResponse<apiResponse>
-  ){
+  req: NextApiRequest,
+  res: NextApiResponse<apiResponse>
+) {
+  const { uri }: { [uri: string]: string | string[] } = req.query;
 
-    const { uri }: { [uri: string]: string | string[] } = req.query;
-
-    const [GETfileApiResponse, GETfileStatus] = await getCMSfile(
-        String(uri)
-      );
+  switch (req.method) {
+    case "GET":
+      const [GETfileApiResponse, GETfileStatus] = await getCMSfile(String(uri));
       return res.status(GETfileStatus).json(GETfileApiResponse);
+
+    case "PUT":
+      const [PUTapiResponse, PUTstatus] = await updateCMSfile(
+        String(uri),
+        req.body
+      );
+      return res.status(PUTstatus).json(PUTapiResponse);
+    default:
+  }
 }
--- pages/api/database/index.ts
+++ pages/api/database/index.ts
@@ -2,7 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import getTablesData from "../../../lib/models/getTablesData";
 import createCMSTable from "../../../lib/models/createCMSTable";
 import deleteCMSTable from "../../../lib/models/deleteCMSTable";
-import updateCMSfile from "../../../lib/models/updateCMSfile";
 
 interface apiResponse {
   success: boolean;
@@ -14,10 +13,6 @@ export default async function Handler(
   res: NextApiResponse<apiResponse>
 ) {
   const { uri }: { [uri: string]: string | string[] } = req.query;
-  const { body }: any = req.body;
-  console.log("Body here:", body)
-
-
 
   switch (req.method) {
     case "GET":
@@ -27,10 +22,6 @@ export default async function Handler(
     case "POST":
       const [POSTapiResponse, POSTstatus] = await createCMSTable(String(uri));
       return res.status(POSTstatus).json(POSTapiResponse);
-
-    case "PUT":
-        const [PUTapiResponse, PUTstatus] = await updateCMSfile(String(uri), body)
-        return res.status(PUTstatus).json(PUTapiResponse)
 
     case "DELETE":
       const [DELETEapiResponse, DELETEstatus] = await deleteCMSTable(
